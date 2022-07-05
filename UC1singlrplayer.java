6d60ba30538ee02ebed5c878c6d722999f6063dd
yogeshdhangar1 <yogeshdhangar5213@gmail.com>
2022-07-05 10:26:39 -0700

    Added java file

---
+++ UC1singlrplayer.java
@@ -0,0 +1,11 @@
+package javaparatice;
+
+public class UC1singlrplayer {
+	public static void main(String[] args) {
+		System.out.println("Wellcome to Snack & Ladder Game :- )");
+		//Variable
+		int startPosition =0;
+		System.out.println("initial position of the player is :-" +startPosition);
+	}
+
+}
