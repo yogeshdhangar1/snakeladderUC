2ead22425489cd2148f886b46ef2f8fb4b5c38a2
yogeshdhangar1 <yogeshdhangar5213@gmail.com>
2022-07-05 20:17:51 -0700

    Added UC^snakeladde

--- UC6snakeladde.java
+++ UC6snakeladde.java
@@ -1,16 +1,18 @@
-package javaproblem;
+package javapractice;
 
-public class UC5snakeladdercase {
-	System.out.println("Welcome to Snake & Ladder Game");
+public class UC6snakeladde {
+	public static void main(String[] args) {
+		
+ 	System.out.println("Welcome to Snake & Ladder Game");
 	//Variable declaring position of player
 	int position = 0;
 	int Ladder = 1;
 	int Snake =2;
 	//int noplay =3;
 	int WinPosition = 100;
-
+    int counter = 0;
 	System.out.println("initial position of the player :-"   +position);
-	while (position<=WinPosition) {
+	 while (counter<=WinPosition )  {
 		if(position==100) {break;}
 		int rollDice = (int)Math.floor(Math.random()*10 %6+1);
 		int option = (int)Math.floor(Math.random()* 10 %3)+1;
@@ -34,5 +36,8 @@ public class UC5snakeladdercase {
 			position =  position +0;
 		}
 		System.out.println("The current Position of the player = " +position);
-	}  
+	 }
+	}
 }
+
+
